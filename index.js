5e7a7a60b911c648800b5c1591538d5301ea0539
JPiantini98 <45038839+JPiantini98@users.noreply.github.com>
2021-02-03 16:52:25 -0400

    Prueba 1 Creation

---
+++ index.js
@@ -0,0 +1,94 @@
+//FACTORY FUNCTION FOR EMPLOYEES
+const employees = (id, name, gender, location, email, phone, cell, picture) => {
+    return {
+        id,
+        name,
+        gender,
+        location,
+        email,
+        phone,
+        cell,
+        id,
+        picture,
+    }
+}
+
+//EMPLOYEES ARRAY
+let employeesList = [];
+
+
+//TABLE CREATION
+function employeesTable (table ,employeesList) {
+    let keys = Object.keys(employeesList[0]);
+
+    //CREATION OF TABLE HEAD
+    let thead = table.createTHead();
+    thead.className = "thead";
+    let row = thead.insertRow();
+
+    for(let key of keys){
+        let th = document.createElement("th");
+        th.className = "header";
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+
+    //CREATION OF TABLE BODY
+    let tbody = table.createTBody();
+    for (let element of employeesList) {
+        let row = tbody.insertRow(); 
+        for (key in element) {
+          let cell = row.insertCell();
+          cell.className="row"          
+          let text = document.createTextNode(element[key]);
+          cell.appendChild(text);
+        }
+    }
+}
+
+/*
+function dataTable(list){
+    $(document).ready(function() {
+        $('#tables').DataTable( {
+            data: list,
+            columns: [
+                { title: "Id" },
+                { title: "Name" },
+                { title: "Gender" },
+                { title: "Location" },
+                { title: "Email" },
+                { title: "Phone" },
+                { title: "Call"},
+                { title: "Picture"}
+            ]
+        } );
+    } );
+}
+*/
+
+//FETCH REQUEST
+async function getEmployees () {
+    let response = await fetch("https://randomuser.me/api/?results=20");
+    let json = await response.json();
+    for (let person in json.results) {
+        employeesList.push(
+            employees(
+                json.results[person].id.value,
+                json.results[person].name.first + " "+json.results[person].name.last,
+                json.results[person].gender,                
+                json.results[person].location.city,
+                json.results[person].email,
+                json.results[person].phone,
+                json.results[person].cell,
+                json.results[person].picture.medium                
+            )
+        );
+    }
+
+    let table = document.getElementById('table');
+    employeesTable(table, employeesList);
+    //dataTable(employeesList);
+}
+
+getEmployees();
